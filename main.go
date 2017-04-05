e561a785414833e132c947ff39349666ee038d55
Denis Rudov <denis.rudov@gmail.com>
2017-04-05 07:15:03 +0300

    service/cli

--- main.go
+++ main.go
@@ -2,21 +2,59 @@ package main
 
 import (
 	"names/api"
+	"github.com/gin-gonic/gin"
+	"sync"
+	"net/http"
+	"flag"
 	"fmt"
 )
 
+var port int
+
+func init() {
+	flag.IntVar(&port, "port", 3000, "service port")
+	flag.Parse()
+}
+
 func main() {
 
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+
+	r.GET("/", getHandler)
+
+	r.Run(fmt.Sprintf(":%d", port))
+
+}
+
+func getHandler(c *gin.Context) {
 	service := api.NewService()
+	var data api.Result
+	wg := sync.WaitGroup{}
+	wg.Add(2)
 
-	randomName, err := service.GetName()
-	chukJoke, err := service.GetNorris()
+	go func() {
+		randomName, err := service.GetName()
+		if err != nil {
+			data.Name = api.Name{}
+		} else {
+			data.Name = randomName
+		}
 
-	if err != nil {
-		fmt.Println(err)
-	}
+		wg.Done()
+	}()
 
-	fmt.Println(randomName.Name)
-	fmt.Println(chukJoke.Value.Joke)
+	go func() {
+		chukJoke, err := service.GetNorris()
+		if err != nil {
+			data.ChuckNorris = api.ChuckNorris{}
+		} else {
+			data.ChuckNorris = chukJoke
+		}
+		wg.Done()
+	}()
 
+	wg.Wait()
+
+	c.JSON(http.StatusOK, data)
 }
